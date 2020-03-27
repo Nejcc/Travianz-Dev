5fdef19d79bc3755af8f9ffcddd80b2d73c5a280
Nejc <nejc.cotic@gmail.com>
2020-03-27 21:22:24 +0100

    update on front-end

---
+++ resources/js/components/ResourceComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <table cellpadding="1" cellspacing="1">
+            <tbody>
+            <tr>
+                <td>
+                    <img src="http://127.0.0.1:8000/images/x.gif" class="r1" alt="Lumber" title="Lumber">
+                </td>
+                <td id="l4" title="12">750/800</td>
+                <td>
+                    <img src="http://127.0.0.1:8000/images/x.gif" class="r2" alt="Clay" title="Clay">
+                </td>
+                <td id="l3" title="8">750/800</td>
+                <td>
+                    <img src="http://127.0.0.1:8000/images/x.gif" class="r3" alt="Iron" title="Iron">
+                </td>
+                <td id="l2" title="8">750/800</td>
+                <td>
+                    <img src="http://127.0.0.1:8000/images/x.gif" class="r4" alt="Crop" title="Crop">
+                </td>
+                <td id="l1" title="12">750/800</td>
+                <td>
+                    <img src="http://127.0.0.1:8000/images/x.gif" class="r5" alt="Crop consumption"
+                         title="Crop consumption">
+                </td>
+                <td>0/12</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,43 +17,50 @@
     @yield('css')
 </head>
 <body class="v35 ie ie8">
-<div class="wrapper">
-    <img class="c1" src="{{ asset('images/x.gif') }}" id="msfilter"/>
-    <div id="dynamic_header"></div>
-    @include('components.header')
-    <div id="mid">
-        @include('components.menu')
-        @yield('content')
-        <br/><br/><br/><br/>
-        <div id="side_info">
-            @if (Auth::check())
-                @include('village.list')
-                {{-- quest --}}
-            @endif
+<div id="app">
+    <div class="wrapper">
+        <img class="c1" src="{{ asset('images/x.gif') }}" id="msfilter"/>
+        <div id="dynamic_header"></div>
+        @include('components.header')
+        <div id="mid">
+            @include('components.menu')
+            @yield('content')
+            <br/><br/><br/><br/>
+            <div id="side_info">
+                @if (Auth::check())
+                    @include('village.list')
+                    {{-- quest --}}
+                @endif
 
-            @if (config('server.newsboxes.1'))
-                @include('newsboxes.newsbox1')
-            @endif
+                @if (config('server.newsboxes.1'))
+                    @include('newsboxes.newsbox1')
+                @endif
 
-            @if (config('server.newsboxes.2'))
-                @include('newsboxes.newsbox2')
-            @endif
+                @if (config('server.newsboxes.2'))
+                    @include('newsboxes.newsbox2')
+                @endif
 
-            @if (config('server.newsboxes.3'))
-                @include('newsboxes.newsbox3')
-            @endif
+                @if (config('server.newsboxes.3'))
+                    @include('newsboxes.newsbox3')
+                @endif
+            </div>
+            <div class="clear"></div>
+{{--            @game_footer--}}
         </div>
         <div class="clear"></div>
-        @game_footer
+{{--        @if (Auth::check())--}}
+            <div id="res">
+                <div id="resWrap">
+                    <resource-component/>
+                </div>
+            </div>
+            @include('village.resources')
+{{--        @endif--}}
+        @include('layout.time')
     </div>
-    <div class="clear"></div>
-    @if (Auth::check())
-        @include('village.resources')
-    @endif
-    @include('layout.time')
+    <div id="ce"></div>
+    @yield('modal')
+    @yield('js')
 </div>
-<div id="ce"></div>
-@yield('modal')
-@yield('js')
 </body>
 </html>
